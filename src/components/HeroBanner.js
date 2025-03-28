ba7ac4d7f0ddba3605fa69006b41b3d1dafa44d0
Zachariah Hassan <hassa844@umn.edu>
2025-03-28 11:40:46 -0500

    fix: broken links

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -3,8 +3,15 @@ import { Box, Stack, Typography, Button } from '@mui/material';
 import HeroBannerImg from '../assets/images/banner.png';
 
 const HeroBanner = () => {
+  const scrollToRecipes = () => {
+    const element = document.getElementById('recipes');
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
   return (
-    <Box 
+    <Box
       sx={{mt: {lg: '212px',xs: '70px'}, ml: {sm: '50px'}}}
       position='relative'
       p='20px'
@@ -15,12 +22,30 @@ const HeroBanner = () => {
       <Typography fontWeight={700}
         sx={{fontSize: {lg: '44px',xs: '40px'}}} mb='23px' mt='30px'
       >
-       Discover new <br /> recipes 
+       Discover new <br /> recipes
       </Typography>
       <Stack>
-        <a href="#recipes" style={{ marginTop: '45px', textDecoration: 'none', width: '200px', textAlign: 'center', background: '#0A6847', padding: '14px', fontSize: '22px', textTransform: 'none', color: 'white', borderRadius: '4px' }}>Explore Recipes</a>
+        <Button
+          onClick={scrollToRecipes}
+          sx={{
+            marginTop: '45px',
+            width: '200px',
+            textAlign: 'center',
+            background: '#0A6847',
+            padding: '14px',
+            fontSize: '22px',
+            textTransform: 'none',
+            color: 'white',
+            borderRadius: '4px',
+            '&:hover': {
+              background: '#085438'
+            }
+          }}
+        >
+          Explore Recipes
+        </Button>
       </Stack>
-      
+
       <Typography fontWeight={600} color='#FFDB5C' sx={{opacity: 0.2,display: {lg: 'block', xs: 'none'}}} fontSize="200px">recipes</Typography>
       <img src={HeroBannerImg} alt='banner' className='hero-banner-img' />
     </Box>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,14 +2,22 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { Stack } from '@mui/material'
 import Logo from '../assets/images/logo.png';
+
 const Navbar = () => {
+  const scrollToRecipes = () => {
+    const element = document.getElementById('recipes');
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
   return (
-    <Stack 
+    <Stack
         direction='row'
         justifyContent='space-around'
         sx={{gap: {sm: '122px', xs: '40px'}, mt: {sm: '32px', xs: '20px'}, justifyContent: 'none'}}
         px='20px'
-        >   
+        >
         <Link to='/'>
             <img src={Logo} alt="logo" style={{width: '48px', height: '48px', margin: '0 20px'}} />
         </Link>
@@ -20,7 +28,20 @@ const Navbar = () => {
           alignItems='flex-end'
         >
             <Link to='/' style={{textDecoration: 'none', color: '#3A1212', borderBottom: '3px solid #75A47F '}}>Home</Link>
-            <a href="#recipes" style={{textDecoration: 'none', color: '#3A1212'}}>Recipes</a>
+            <button
+              onClick={scrollToRecipes}
+              style={{
+                textDecoration: 'none',
+                color: '#3A1212',
+                background: 'none',
+                border: 'none',
+                padding: 0,
+                font: 'inherit',
+                cursor: 'pointer'
+              }}
+            >
+              Recipes
+            </button>
         </Stack>
     </Stack>
   )
